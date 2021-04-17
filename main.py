abb3f02fa73152ffaf033de37637660c8290c43d
nay-uku <rhag.nay@protonmail.com>
2021-04-17 15:44:59 +0200

    w1-7 - PD

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
-from fastapi import FastAPI
+import hashlib
+
+from fastapi import FastAPI, Request, Response, status, HTTPException
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel
+from typing import Optional
+from datetime import datetime, timedelta
 
 app = FastAPI()
 app.counter = 0
+app.patient_id = 1
+app.patients = []
+app.ids = []
 
 
 class HelloResp(BaseModel):
     msg: str
 
 
-@app.get("/")
-def root():
-    return {"message": "Hello World"}
+class Patient(BaseModel):
+    name: str
+    surname: str
 
 
 @app.get("/counter")
@@ -23,3 +32,57 @@ def counter():
 @app.get("/hello/{name}", response_model=HelloResp)
 def test_hello_name(name: str):
     return HelloResp(msg=f"Hello {name}")
+
+
+# 1.1
+@app.get("/")
+def root():
+    return {"message": "Hello world!"}
+
+
+# 1.2
+@app.api_route("/method", methods=["GET", "POST", "DELETE", "PUT", "OPTIONS"], status_code=200)
+def method_type(request: Request, response: Response):
+    if request.method == "POST":
+        response.status_code = status.HTTP_201_CREATED
+    return {"method": request.method}
+
+
+# 1.3
+@app.get("/auth", status_code=401)
+def check_password(password: Optional[str] = None, password_hash: Optional[str] = None):
+    if not password or not password_hash:
+        return
+
+    calc_hash = hashlib.sha512(password.encode("utf-8")).hexdigest()
+    if calc_hash == password_hash:
+        return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+# 1.4
+@app.post("/register")
+def register(patient: Patient):
+    register_date = datetime.now().strftime("%Y-%m-%d")
+    vaccination_date = datetime.strptime(register_date, "%Y-%m-%d") + timedelta(
+        len(patient.name) + len(patient.surname))
+    vaccination_date = vaccination_date.strftime("%Y-%m-%d")
+    patient_info = {
+        "id": app.patient_id,
+        "name": patient.name,
+        "surname": patient.surname,
+        "register_date": register_date,
+        "vaccination_date": vaccination_date
+    }
+    app.patients.append(patient_info)
+    app.patient_id += 1
+    return patient_info
+
+
+# 1.5
+@app.get("/patient/{id}")
+def get_patient_info(id: int):
+    if id < 1:
+        raise HTTPException(status_code=400)
+    elif id > len(app.patients):
+        raise HTTPException(status_code=404)
+    return app.patients[id - 1]
--- tests.py
+++ tests.py
@@ -1,7 +1,6 @@
 import pytest
 from fastapi.testclient import TestClient
-
-from main import app
+from main import app, datetime, timedelta
 
 client = TestClient(app)
 
@@ -9,7 +8,7 @@ client = TestClient(app)
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello World"}
+    assert response.json() == {"message": "Hello world!"}
 
 
 @pytest.mark.parametrize("name", ["Zenek", "Mario", "Abc Def"])
@@ -24,3 +23,81 @@ def test_counter():
         response = client.get("/counter")
         assert response.status_code == 200
         assert response.text == str(i)
+
+
+def test_method_type_get():
+    response = client.get("/method")
+    assert response.status_code == 200
+    assert response.json() == {"method": "GET"}
+
+
+def test_method_type_post():
+    response = client.post("/method")
+    assert response.status_code == 201
+    assert response.json() == {"method": "POST"}
+
+
+def test_method_type_delete():
+    response = client.delete("/method")
+    assert response.status_code == 200
+    assert response.json() == {"method": "DELETE"}
+
+
+def test_method_type_put():
+    response = client.put("/method")
+    assert response.status_code == 200
+    assert response.json() == {"method": "PUT"}
+
+
+def test_method_type_options():
+    response = client.options("/method")
+    assert response.status_code == 200
+    assert response.json() == {"method": "OPTIONS"}
+
+
+def test_check_password():
+    response = client.get(
+        "/auth?password=haslo&password_hash=013c6889f799cd986a735118e1888727d1435f7f623d05d58c61bf2cd8b49ac90105e5786ce"
+        "aabd62bbc27336153d0d316b2d13b36804080c44aa6198c533215")
+    assert response.status_code == 204
+    response = client.get(
+        "/auth?password=haslo&password_hash=f34ad4b3ae1e2cf33092e2abb60dc0444781c15d0e2e9ecdb37e4b14176a0164027b05900e0"
+        "9fa0f61a1882e0b89fbfa5dcfcc9765dd2ca4377e2c794837e091")
+    assert response.status_code == 401
+
+
+def test_register():
+    patient = {
+        "name": "Jan",
+        "surname": "Kowalski"
+    }
+    response = client.post("/register", json=patient)
+    vaccination_date = datetime.now() + timedelta(len(patient["name"]) + len(patient["surname"]))
+    vaccination_date = vaccination_date.strftime("%Y-%m-%d")
+    assert response.json() == {
+        "id": 1,
+        "name": "Jan",
+        "surname": "Kowalski",
+        "register_date": datetime.now().strftime("%Y-%m-%d"),
+        "vaccination_date": vaccination_date
+    }
+
+
+def test_get_patient_info():
+    # < 0
+    response = client.get("/patient/0")
+    assert response.status_code == 400
+
+    # > num of patients
+    response = client.get(f"/patient/{len(app.patients)+1}")
+    assert response.status_code == 404
+
+    # return first patient
+    patient = {
+        "name": "Jan",
+        "surname": "Kowalski"
+    }
+    client.post("/register", json=patient)
+    response = client.get(f"/patient/1")
+    assert response.status_code == 200
+    assert response.json() == app.patients[0]
